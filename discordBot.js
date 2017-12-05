5722e6770fbe02ba2cd31755070184196adcf616
Avery Vine <averyvine@icloud.com>
2017-12-04 19:13:39 -0500

    base for youtube searching

--- discordBot.js
+++ discordBot.js
@@ -1,5 +1,6 @@
 const apiKeys = require('./apiKeys.js');
 const league = require('./league.js');
+const youtube = require('./youtube.js');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const botName = "alfred";
@@ -13,12 +14,14 @@ client.on('message', message => {
     if (!message.author.bot) {
         if (message.content.toLowerCase().startsWith(botName + " ")) {
             var content = message.content.replace(botName + " ", "").trim();
-            console.log("Content: " + content);
             if (content === 'ping') {
                 message.channel.send('pong');
             }
             else if (content.startsWith('summoner ')) {
                 league.summoner(message, content);
+            }
+            else if (content.startsWith('youtube ')) {
+                youtube.search(message, content);
             }
         }
     }
---
+++ youtube.js
@@ -0,0 +1,8 @@
+const apiKeys = require('./apiKeys.js');
+const request = require('request');
+const Discord = require('discord.js');
+const $ = module.exports;
+
+$.search = function(messageObject, content) {
+    console.log("Searching YouTube for: " + content);
+}
