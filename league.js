463966fa36b89dfb8c2013cdd1ca488670fbd448
Avery Vine <averyvine@icloud.com>
2017-12-07 00:32:28 -0500

    added twitch search integration

--- league.js
+++ league.js
@@ -6,6 +6,7 @@ const $ = module.exports;
 var summoner = null;
 var version = null;
 var message = null;
+var options = {};
 
 class Summoner {
     constructor(name, id, accountId, profileIconId, summonerLevel) {
@@ -28,89 +29,103 @@ $.summoner = function (messageObject, content) {
     }
     var summonerName = content;
     console.log("Looking up " + summonerName);
-    var url = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + encodeURIComponent(summonerName) + '?api_key=' + apiKeys.riot;
-    console.log("Reaching out to " + url);
-    request(url, function (error, response, body) {
-        switch (response.statusCode) {
-            case 200:
-                var json = JSON.parse(body);
-                summoner = new Summoner(json.name, json.id, json.accountId, json.profileIconId, json.summonerLevel);
-                getSummonerIcon();
-                break;
-            case 404:
-                console.log("Summoner " + summonerName + " not found.");
-                sendMessage("Invalid summoner name!");
-                break;
-            case 429:
-                console.log("Rate limit exceeded");
-                sendMessage("Too many requests! Please try again later.");
-                break;
-            default:
-                console.log("Something went wrong.\nCode: " + response.statusCode + "\nBody: " + body);
-                sendMessage("Something went wrong!");
-        }
-    });
+    options.url = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + encodeURIComponent(summonerName);
+    options.headers = {
+        "X-Riot-Token": apiKeys.riot
+    };
+    console.log("Reaching out to " + options.url);
+    request(options, summonerCallback);
+}
+
+function summonerCallback(error, response, body) {
+    switch (response.statusCode) {
+        case 200:
+            var json = JSON.parse(body);
+            summoner = new Summoner(json.name, json.id, json.accountId, json.profileIconId, json.summonerLevel);
+            getSummonerIcon();
+            break;
+        case 404:
+            console.log("Summoner not found\nCode: " + response.statusCode + "\nBody: " + body);
+            sendMessage("Invalid summoner name!");
+            break;
+        case 429:
+            console.log("Rate limit exceeded\nCode: " + response.statusCode + "\nBody: " + body);
+            sendMessage("Too many requests! Please try again later.");
+            break;
+        default:
+            console.log("Something went wrong.\nCode: " + response.statusCode + "\nBody: " + body);
+            sendMessage("Something went wrong!");
+    }
 }
 
 function getSummonerIcon() {
-    var url = 'https://na1.api.riotgames.com/lol/static-data/v3/versions?api_key=' + apiKeys.riot;
-    console.log("Reaching out to " + url);
-    request(url, function (error, response, body) {
-        switch (response.statusCode) {
-            case 200:
-                var json = JSON.parse(body);
-                version = json[0];
-                getRank();
-                break;
-            case 429:
-                console.log("Rate limit exceeded");
-                sendMessage("Too many requests! Please try again later.");
-                break;
-            default:
-                console.log("Something went wrong.\nCode: " + response.statusCode + "\nBody: " + body);
-                sendMessage("Something went wrong!");
-        }
-    });
+    options.url = 'https://na1.api.riotgames.com/lol/static-data/v3/versions';
+    options.headers = {
+        "X-Riot-Token": apiKeys.riot
+    };
+    console.log("Reaching out to " + options.url);
+    request(options, summonerIconCallback);
+}
 
+function summonerIconCallback(error, response, body) {
+    switch (response.statusCode) {
+        case 200:
+            var json = JSON.parse(body);
+            version = json[0];
+            getRank();
+            break;
+        case 429:
+            console.log("Rate limit exceeded\nCode: " + response.statusCode + "\nBody: " + body);
+            sendMessage("Too many requests! Please try again later.");
+            break;
+        default:
+            console.log("Something went wrong.\nCode: " + response.statusCode + "\nBody: " + body);
+            sendMessage("Something went wrong!");
+    }
 }
 
 function getRank() {
     summoner.rank = [];
-    var url = 'https://na1.api.riotgames.com/lol/league/v3/leagues/by-summoner/' + summoner.id + '?api_key=' + apiKeys.riot;
-    console.log("Reaching out to " + url);
-    request(url, function (error, response, body) {
-        switch (response.statusCode) {
-            case 200:
-                var json = JSON.parse(body);
-                for (i in json) {
-                    var queue = json[i];
-                    summoner.rank.push({
-                        name: queue.queue.split("_").join(" "),
-                        tier: queue.tier,
-                        rank: null,
-                        leaguePoints: 0
-                    });
-                    for (j in queue.entries) {
-                        var player = queue.entries[j];
-                        if (player.playerOrTeamId == summoner.id) {
-                            summoner.rank[i].rank = player.rank;
-                            summoner.rank[i].leaguePoints = player.leaguePoints;
-                            break;
-                        }
+    options.url = 'https://na1.api.riotgames.com/lol/league/v3/leagues/by-summoner/' + summoner.id;
+    options.headers = {
+        "X-Riot-Token": apiKeys.riot
+    };
+    console.log("Reaching out to " + options.url);
+    request(options, rankCallback);
+}
+
+function rankCallback(error, response, body) {
+    switch (response.statusCode) {
+        case 200:
+            var json = JSON.parse(body);
+            for (i in json) {
+                var queue = json[i];
+                summoner.rank.push({
+                    name: queue.queue.split("_").join(" "),
+                    tier: queue.tier,
+                    rank: null,
+                    leaguePoints: 0
+                });
+                for (j in queue.entries) {
+                    var player = queue.entries[j];
+                    if (player.playerOrTeamId == summoner.id) {
+                        summoner.rank[i].rank = player.rank;
+                        summoner.rank[i].leaguePoints = player.leaguePoints;
+                        break;
                     }
                 }
-                console.log("Rank: " + summoner.rank.toString());
-                sendSummonerEmbedded();
-                break;
-            case 429:
-                console.log("Rate limit exceeded");
-                sendMessage("Too many requests! Please try again later.");
-                break;
-            default:
-                console.log("Something went wrong.\nCode: " + response.statusCode + "\nBody: " + body);
-                sendMessage("Something went wrong!");
-        }
-    });
+            }
+            console.log("Rank: " + summoner.rank.toString());
+            sendSummonerEmbedded();
+            break;
+        case 429:
+            console.log("Rate limit exceeded\nCode: " + response.statusCode + "\nBody: " + body);
+            sendMessage("Too many requests! Please try again later.");
+            break;
+        default:
+            console.log("Something went wrong.\nCode: " + response.statusCode + "\nBody: " + body);
+            sendMessage("Something went wrong!");
+    }
 }
 
 function sendSummonerEmbedded() {
@@ -134,7 +149,7 @@ function sendSummonerEmbedded() {
         fields: embedFields,
         footer: {
             icon_url: "https://cdn.discordapp.com/embed/avatars/0.png",
-            text: "I am a bot."
+            text: "I am a bot, beep boop."
         },
     };
     embedData.timestamp = new Date();
